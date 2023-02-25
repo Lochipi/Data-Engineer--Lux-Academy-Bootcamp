2d292306c13b4f764e0dbfba31ec1b6c1fc61b70
Cornelius Emase <corneliuslochipi@gmail.com>
2023-02-25 11:57:42 +0300

    added python task

---
+++ week 2/tasks/python_task.py
@@ -0,0 +1,4 @@
+# python task
+#  Python. Given an array of integers, find the sum of its elements.
+# 	For example, if the array ar = [1,2,3],
+#                           Calculate 1+2+3 = 6, so return 6.
