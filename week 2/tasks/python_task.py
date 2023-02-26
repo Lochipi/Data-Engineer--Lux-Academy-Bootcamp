4394060ddb25ad91ab1604d278acb6b62dcb3c0c
Cornelius Emase <corneliuslochipi@gmail.com>
2023-02-26 22:57:08 +0300

    added python solution

--- week 2/tasks/python_task.py
+++ week 2/tasks/python_task.py
@@ -1,4 +1,23 @@
 # python task
-#  Python. Given an array of integers, find the sum of its elements.
+#Given an array of integers, find the sum of its elements.
 # 	For example, if the array ar = [1,2,3],
 #                           Calculate 1+2+3 = 6, so return 6.
+
+# pseudocode
+
+# define a function that takes an argument that takes the inputs
+# create a variable sum and set it to zero(0)
+# create a loop to iterate through the array 
+# add each element to the sum variable one at a time
+
+# arr = [1,2,3]
+
+
+def sumOfArray(arr):
+    sum = 0
+    for i in arr:
+        sum = sum + i
+    return(sum)    
+
+print(sumOfArray([1,2,3]))
+
