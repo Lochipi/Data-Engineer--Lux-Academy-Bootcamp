213574767dda4cbe7fc680b2dfd0671694f339dc
Cornelius Emase <corneliuslochipi@gmail.com>
2023-03-03 23:19:52 +0300

    added week 3 folder with objectives

---
+++ week 2/week 3/objectives_notes.py
@@ -0,0 +1,17 @@
+# This week let's end the week with a deeper dive into flask:
+# 1). How to write a Python web API with Flask
+# https://opensource.com/article/19/11/python-web-api-flask
+# 2). Developing Web Applications with Flask
+# https://www3.ntu.edu.sg/home/ehchua/programming/webprogramming/Python3_Flask.html
+# 3). Creating RESTful Web APIs using Flask and Python
+# https://towardsdatascience.com/creating-restful-apis-using-flask-and-python-655bad51b24
+# Also go through docker documentation, so that we can learn how to document our flask apps and ml projects to work on any operating system. https://www.docker.com/products/docker-desktop/
+
+# https://towardsdatascience.com/creating-restful-apis-using-flask-and-python-655bad51b24   ==> A Comprehensive Guide for building Web APIs with Flask
+
+# Sometimes in your career you meet that you need to use R programming Language hence i have provided two resources you  can use to get started with R Programming Language, https://www.datamentor.io/r-programming/ and  https://www.w3schools.com/r/r_intro.asp
+
+# Learning data resources == datasets
+# https://www.earthdata.nasa.gov/
+
+# sql murder mystery ==> https://mystery.knightlab.com/
