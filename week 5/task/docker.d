bc43892aae6c29fe46294cfd955c3566e35d355c
Cornelius Emase <corneliuslochipi@gmail.com>
2023-03-18 16:42:46 +0300

    added docker assignments

---
+++ week 5/task/docker.d
@@ -0,0 +1,10 @@
+documentation
+https://docs.docker.com/get-docker/ 
+
+do some docker demio
+
+https://docs.docker.com/get-started/
+
+q. what i have learnt by monday 15 min
+
+project done sato https://www.kaggle.com/code/athilkar/cars-data-analysis-eda
